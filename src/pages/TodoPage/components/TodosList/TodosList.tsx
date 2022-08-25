785edd8e3fd7e9f5c0b7430307a497babbd26ff7
AlfredoPrograma <alfredoprograma.dev@gmail.com>
2022-08-25 11:14:47 -0400

    [FEAT]: add TodoList header

--- src/pages/TodoPage/components/TodosList/TodosList.tsx
+++ src/pages/TodoPage/components/TodosList/TodosList.tsx
@@ -7,7 +7,20 @@ const TodosList = () => {
 
   return (
     <section className={styles.todosList}>
-      { todos.map(_todo => <TodoCard key={_todo.id} todo={_todo} />)}
+      <header>
+        <h1 className={styles.header}>Todo App</h1>
+      </header>
+
+      <div className={styles.container}>
+        {
+          !todos.length
+            ? <div className={styles.emptyTodoState}>
+                <h2>No todos added yet</h2>
+                <h3>Click the button to add your first TODO!!!</h3>
+              </div>
+            : todos.map(_todo => <TodoCard key={_todo.id} todo={_todo} />)
+        }
+      </div>
     </section>
   )
 }
